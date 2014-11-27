83984e9f398b9ecebd83e0bf949a91b580cc6112
Pierluigi Cifani <piercifani@gmail.com>
2014-11-27 17:57:05 +0100

    Now you can view the detail of the delivery and mark it there as done!

--- DeliveryBoy WatchKit Extension/DeliveryListInterfaceController.swift
+++ DeliveryBoy WatchKit Extension/DeliveryListInterfaceController.swift
@@ -10,6 +10,16 @@ import WatchKit
 import Foundation
 import DeliveryKit
 
+class DeliverySegueHolder {
+    let manager : DeliverManager
+    let delivery : Delivery
+    
+    init (manager : DeliverManager, delivery : Delivery) {
+        self.manager = manager
+        self.delivery = delivery
+    }
+}
+
 class DeliveryTableRowController : NSObject {
     @IBOutlet weak var addressSample: WKInterfaceLabel!
 }
@@ -20,6 +30,7 @@ class DeliveryListInterfaceController: WKInterfaceController {
     let manager = DeliverManager()
     var deliveries : [Delivery]?
 
+    //MARK: WKInterfaceController
     override init(context: AnyObject?) {
         // Initialize variables here.
         super.init(context: context)
@@ -39,26 +50,82 @@ class DeliveryListInterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        NSLog("%@ will activate", self)
+        self.populateTable()
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
-        NSLog("%@ did deactivate", self)
         super.didDeactivate()
     }
 
+    override func contextForSegueWithIdentifier(segueIdentifier: String, inTable table: WKInterfaceTable, rowIndex: Int) -> AnyObject? {
+        let delivery = self.deliveries![rowIndex]
+        return DeliverySegueHolder(manager: manager, delivery: delivery)
+    }
+    
+    //MARK: Private
     func populateTable() {
-        deliveryTable.setNumberOfRows(self.deliveries!.count, withRowType: "DeliveryTableRowController")
-        for (index, element) in enumerate(self.deliveries!){
-            let delivery = element as Delivery
-            var attributedText : NSMutableAttributedString = NSMutableAttributedString(string:delivery.address)
-            if (delivery.isDelivered()) {
-                attributedText.addAttribute(NSStrikethroughStyleAttributeName, value: 1, range: NSMakeRange(0, attributedText.length))
+        if let _deliveries = self.deliveries {
+            deliveryTable.setNumberOfRows(_deliveries.count, withRowType: "DeliveryTableRowController")
+            for (index, element) in enumerate(_deliveries){
+                let delivery = element as Delivery
+                var attributedText : NSMutableAttributedString = NSMutableAttributedString(string:delivery.address)
+                if (delivery.isDelivered()) {
+                    attributedText.addAttribute(NSStrikethroughStyleAttributeName, value: 1, range: NSMakeRange(0, attributedText.length))
+                }
+                let row = deliveryTable.rowControllerAtIndex(index) as DeliveryTableRowController
+                row.addressSample.setAttributedText(attributedText)
             }
-            let row = deliveryTable.rowControllerAtIndex(index) as DeliveryTableRowController
-            row.addressSample.setAttributedText(attributedText)
         }
     }
 }
 
+class DeliveryDetailInterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var addressMapView: WKInterfaceMap!
+    @IBOutlet weak var addressLabel: WKInterfaceLabel!
+    @IBOutlet weak var deliveryTimeLabel: WKInterfaceLabel!
+    @IBOutlet weak var doneButton: WKInterfaceButton!
+    var manager : DeliverManager!
+    let delivery: Delivery!
+    
+    override init(context: AnyObject?) {
+        super.init(context: context)
+        if let _segueHolder = context as? DeliverySegueHolder {
+            delivery = _segueHolder.delivery
+            manager = _segueHolder.manager
+        }
+    }
+
+    @IBAction func onDone() {
+        manager.markDeliveryAsDone(delivery) {
+            (success, error) -> Void in
+            self.prepareForDelivery()
+        }
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        self.prepareForDelivery()
+    }
+    
+    func prepareForDelivery() {
+        let deliveryAddressText : NSMutableAttributedString = NSMutableAttributedString(string:delivery.address)
+
+        if delivery.isDelivered() {
+            self.doneButton.setHidden(true)
+            self.deliveryTimeLabel.setHidden(false)
+            self.deliveryTimeLabel.setText("Delivered at \(delivery.deliveryDate!)")
+            deliveryAddressText.addAttribute(NSStrikethroughStyleAttributeName, value: 1, range: NSMakeRange(0, deliveryAddressText.length))
+
+        } else {
+            self.doneButton.setHidden(false)
+            self.deliveryTimeLabel.setHidden(true)
+        }
+        
+        self.addressLabel.setAttributedText(deliveryAddressText)
+        self.addressMapView.setCoordinateRegion(MKCoordinateRegionMake(delivery!.latitudeLongitude, MKCoordinateSpanMake(0.01, 0.01)))
+        self.addressMapView.addAnnotation(delivery!.latitudeLongitude, withPinColor: .Red)
+    }
+}
